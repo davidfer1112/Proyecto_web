2d2910dab7f31b8a844c8c2046d1ea58009821af
davidfer1112 <davidfernando1112@gmail.com>
2023-09-05 01:18:30 -0500

    feature: new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Definicion de la ruta login
+  {path: 'login',component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Proyecto_web';
+
+  constructor(private router: Router){}
+
+
+  iniciarSesion() {
+    console.log('Iniciando sesión...');
+    this.router.navigateByUrl('/login')
+  }
+
+  registrarse() {
+    console.log('Registrándose...');
+  }
+
+
 }
