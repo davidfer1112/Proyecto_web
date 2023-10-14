811d990bf6c8d5eb56e94a78b52e909412bb78da
davidfer1112 <davidfernando1112@gmail.com>
2023-10-14 18:32:26 -0500

    Fix: Imports y animacion FadeIn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgParticlesModule } from "ng-particles";
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ import { InterfazAdminComponent } from './interfaz-admin/interfaz-admin.componen
     BrowserModule,
     AppRoutingModule,
     NgParticlesModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
