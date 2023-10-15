42ac8fa88ad5e6289f1636ec4e9c1106f9e17095
davidfer1112 <davidfernando1112@gmail.com>
2023-10-14 23:46:52 -0500

    Feature: modelo

---
+++ src/app/models/Admin.model.ts
@@ -0,0 +1,5 @@
+
+export interface AdminModel{
+    nombre: string;
+    contrasenia: string;
+}
---
+++ src/app/models/Persona.models.ts
@@ -0,0 +1,7 @@
+
+export interface PersonaModel{
+    apellido: string;
+    contrasenia: string;
+    correo_electronico: string;
+    nombre: string;
+}
