bc18a6248ad9d6c574c87a64e9d018f4aeded683
davidfer1112 <davidfernando1112@gmail.com>
2023-10-18 01:31:43 -0500

    Fix: seleccion de me gusta en componente lista

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+
 import { ListaModel } from 'src/app/models/Lista.model';
 
 @Component({
@@ -7,9 +8,9 @@ import { ListaModel } from 'src/app/models/Lista.model';
   styleUrls: ['./listas.component.css']
 })
 
-
 export class ListasComponent {
-  @Input() listas: ListaModel[] = [];
+  @Input() lista: ListaModel = { genero: '', num_likes: 0 };
+
   imagenesLike = [
     'assets/images/gusta.png',
     'assets/images/gustaRelleno.png'
@@ -27,4 +28,3 @@ export class ListasComponent {
     }, 100); 
   }
 }
-
