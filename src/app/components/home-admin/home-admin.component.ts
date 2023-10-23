172665cd034426c721a1771669d3aa6614076f5b
David Fernando Pérez Medina <davidfernando1112@gmail.com>
2023-10-23 17:26:43 -0500

    Fix: error busqueda por segunda vez

--- src/app/components/home-admin/home-admin.component.ts
+++ src/app/components/home-admin/home-admin.component.ts
@@ -4,26 +4,27 @@ import { ListaService } from 'src/app/services/Lista/lista.service';
 import { ListaModel } from 'src/app/models/Lista.model';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-home-admin',
   templateUrl: './home-admin.component.html',
   styleUrls: ['./home-admin.component.css']
 })
-export class HomeAdminComponent {
-  datos: ListaModel[] = [];
+export class HomeAdminComponent implements OnInit {
+  datosOriginales: ListaModel[] = [];
+  datosFiltrados: ListaModel[] = [];
   filtroTexto: string = '';
 
   constructor(
-    private servicio: CancionService, 
-    private serviciolista: ListaService, 
-    private router: Router) {}
-
+    private servicio: CancionService,
+    private serviciolista: ListaService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.serviciolista.getListas().subscribe(
       (response: ListaModel[]) => {
-        this.datos = response;
+        this.datosOriginales = response;
+        this.datosFiltrados = [...this.datosOriginales]; // Copia los datos originales al array filtrado
       },
       (error) => {
         console.error("Error al obtener los datos", error);
@@ -31,24 +32,19 @@ export class HomeAdminComponent {
     );
   }
 
-
+  // Función para filtrar listas segun la entrada en el input de busqueda
   filtrarListas() {
     if (this.filtroTexto.trim() !== '') {
-      this.datos = this.datos.filter(lista =>
+      this.datosFiltrados = this.datosOriginales.filter(lista =>
         lista.genero.toLowerCase().includes(this.filtroTexto.toLowerCase())
       );
     } else {
-      // Si el input está vacío, restaura la lista original
-      this.serviciolista.getListas().subscribe(
-        (response: ListaModel[]) => {
-          this.datos = response;
-        },
-        (error) => {
-          console.error("Error al obtener los datos", error);
-        }
-      );
+      this.datosFiltrados = [...this.datosOriginales];
     }
   }
 
+  get datos(): ListaModel[] {
+    return this.filtroTexto.trim() !== '' ? this.datosFiltrados : this.datosOriginales;
+  }
 
 }
