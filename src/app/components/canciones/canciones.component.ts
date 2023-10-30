c117e6f1753b66ecd3e6cfb561eeeee7cb1ffa5b
David Fernando Pérez Medina <davidfernando1112@gmail.com>
2023-10-29 22:31:14 -0500

    Feature: likes cancion tiempo real

--- src/app/components/canciones/canciones.component.ts
+++ src/app/components/canciones/canciones.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 import { SharedService } from 'src/app/services/Shared/shared.service';
 import { CancionService } from 'src/app/services/Cancion/cancion.service';
 
@@ -7,13 +7,18 @@ import { CancionService } from 'src/app/services/Cancion/cancion.service';
   templateUrl: './canciones.component.html',
   styleUrls: ['./canciones.component.css']
 })
-export class CancionesComponent {
+export class CancionesComponent implements OnInit {
 
   constructor(private sharedService: SharedService, private cancionService: CancionService) {
       this.sharedService.rutaActual$.subscribe((ruta) => {
       this.mostrarBotonLike = ruta !== '/home/admin';
     });
   }
+
+  ngOnInit(): void {
+    this.verificarLike();
+  }
+
 
   @Input() nombre: string = '';
   @Input() duracion: string = '';
@@ -27,21 +32,36 @@ export class CancionesComponent {
 
   indiceImagenActual = 0;
   mostrarBotonLike: boolean = true;
+  tieneLike: boolean = false;
 
   get imagenLike(): string {
     return this.imagenesLike[this.indiceImagenActual];
   }
 
-  cambiarImagen() {
-    setTimeout(() => {
-      this.indiceImagenActual = (this.indiceImagenActual + 1) % this.imagenesLike.length;
-    }, 100);
+  verificarLike() {
+    if (this.nombre) {
+      this.cancionService.getLikeEstadoCancion(this.nombre).subscribe(
+        (response) => {
+          // Asegúrate de que el valor de response sea 1 o 0
+          this.indiceImagenActual = response === 1 ? 1 : 0;
+          console.log('Estado de like:', response);
+        },
+        (error) => {
+          console.error('Error al obtener estado de like', error);
+        }
+      );
+    } else {
+      console.error('Nombre de la canción no definido');
+    }
+  }
   
+  darLike() {
     this.cancionService.getRelacionPersonaCancion(this.nombre).subscribe(
       resultado => {
         // Actualizar el número de likes en el componente
         this.numLikes = resultado.cancionDTO.numLikes;  // Asegúrate de que la estructura de la respuesta sea la correcta
         console.log('Relación persona-canción:', resultado);
+        this.verificarLike(); // Después de dar like, verifica y actualiza la imagen
       },
       error => {
         console.error('Error al obtener la relación persona-canción:', error);
